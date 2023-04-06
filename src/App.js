851f9ffe8965962e87a82320b549ad8ed356714f
Dodajon Khusnitdinov <ddark471@gmail.com>
2023-04-06 21:01:52 +0500

    fiirst commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className='shadow '>
+        <div className="container-fluid py-4">
+          <img src={"https://olcha.uz/_nuxt/logo-red.e48e0ab8.svg"} alt=""/>
+        </div>
+      </nav>
+
+      <div id="cards" className="py-5">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6 col-md-4 col-lg-3">
+              <img className="d-block" src={"https://olcha.uz/image/220x220/products/2022-06-13/smartfon-xiaomi-redmi-note-11-pro-128gb-8-gb-grey-58466-0.jpeg"} alt=""/>
+              <a href="#" className='category__name'>Smart Phones</a>
+              <p className="price">2 290 000 sum</p>
+              <p className="price__month">222 000 sum/ month</p>
+              <p className="title">Xiaomi Redmi 10 4/128GB Black</p>
+              <a href="#" className="btn">Buy</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
